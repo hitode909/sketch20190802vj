8205a5f3b1cb86c4fadec782ad108fdad1f30157
hitode909 <hitode909@gmail.com>
2019-07-14 14:59:32 +0900

    timing

--- 5.js
+++ 5.js
@@ -1,18 +1,23 @@
 const from = document.querySelector('#from');
 const diffInput = document.querySelector('#diff');
+const intervalInput = document.querySelector('#interval');
 const char = document.querySelector('#char');
 
 let i = 0;
+let base = from.value.codePointAt(0);
 const nextEmoji = () => {
-    const base = from.value.codePointAt(0);
+    if (from.value) {
+        base = from.value.codePointAt(0);
+    }
     const diff = + diffInput.value;
     return String.fromCodePoint(base + (i++ % (diff+1)));
 };
 
 const step = () => {
     char.textContent = nextEmoji();
+    setTimeout(() => {
+        step();
+    }, intervalInput.value);
 };
 
-setInterval(() => {
-    step();
-}, 20)
+step();
