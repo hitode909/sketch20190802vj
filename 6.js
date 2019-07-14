38b8b55a53402ea719efd68a923b221359396d3d
hitode909 <hitode909@gmail.com>
2019-07-14 14:29:12 +0900

    auto mode

--- 6.js
+++ 6.js
@@ -3,7 +3,11 @@ const inputs = document.querySelectorAll('input');
 const collectParameters = () => {
     const result = {};
     inputs.forEach(i => {
-        result[i.name] = i.value;
+        if (i.type === 'checkbox') {
+            result[i.name] = i.checked;
+        } else {
+            result[i.name] = i.value;
+        }
     });
     return result;
 }
@@ -23,16 +27,29 @@ function onYouTubeIframeAPIReady() {
     });
 }
 
+const autoMode = () => {
+    const parameters = collectParameters();
+    if (!parameters.autoMode) return;
+    if (Math.random() > 0.3) return;
+
+    document.querySelectorAll('input[type="range"]').forEach((i) => {
+        i.value = Math.random() * i.max;
+    });
+};
+
 let timer;
 const observe = () => {
     if (timer) {
         clearTimeout(timer);
         timer = null;
     }
+    document.querySelector('input[name="seekTo"]').max = player.getDuration();
     const parameters = collectParameters();
     if (parameters.videoId !== new URL(player.getVideoUrl()).searchParams.get('v')) {
         player.loadVideoById(parameters.videoId, + parameters.seekTo);
     }
+
+    autoMode();
     player.seekTo(+ parameters.seekTo);
     player.setPlaybackRate(+ parameters.playbackRate);
     player.playVideo();
@@ -41,7 +58,6 @@ const observe = () => {
 };
 
 function onPlayerReady(event) {
-    document.querySelector('input[name="seekTo"]').max = player.getDuration();
     inputs.forEach(input => {
         input.addEventListener('change', () => {
             observe();
